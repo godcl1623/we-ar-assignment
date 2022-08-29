ec09cc580f1521d22061e3f3184b82483866a0a3
godcl1623 <godcl1623@gmail.com>
2022-08-29 21:32:37 +0900

    test : 웹팩 설정 및 import, export 작동 확인

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var canvas_1 = require("./elements/canvas");
-var main = document.querySelector('#root');
-var canvasInstance = new canvas_1["default"]();
-var canvas = canvasInstance.makeCanvas();
-main === null || main === void 0 ? void 0 : main.appendChild(canvas);
