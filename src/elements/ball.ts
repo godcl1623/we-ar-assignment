8e5221735522210bf6fac03821a0b370cba5ac31
godcl1623 <godcl1623@gmail.com>
2022-09-04 22:47:05 +0900

    #1 feat : 기본적인 애니메이션 공에 부여

--- src/elements/ball.ts
+++ src/elements/ball.ts
@@ -7,12 +7,12 @@ interface BallAttributes {
 }
 
 class Ball {
-  context: CanvasRenderingContext2D | null;
-  xCoordinates: number;
-  yCoordinates: number;
-  radius: number;
-  xSpeed: number;
-  ySpeed: number;
+  private context: CanvasRenderingContext2D | null;
+  private xCoordinates: number;
+  private yCoordinates: number;
+  private radius: number;
+  private xSpeed: number;
+  private ySpeed: number;
 
   constructor(ballAttributes: BallAttributes) {
     this.context = ballAttributes.context;
@@ -21,32 +21,62 @@ class Ball {
     this.yCoordinates = this.correctYCoordinates(ballAttributes.yCoordinates);
     this.xSpeed = ballAttributes.speed;
     this.ySpeed = ballAttributes.speed;
-    console.log(this.xCoordinates + this.radius);
   }
 
   draw() {
     if (this.context != null) {
+      const { minimumX, minimumY, maximumX, maximumY } = this.getContextBoundaries();
       this.context.beginPath();
       this.context.fillStyle = 'black';
       this.context.arc(this.xCoordinates, this.yCoordinates, this.radius, 0, 2 * Math.PI, false);
       this.context.fill();
+
+      if (this.xCoordinates <= minimumX || this.xCoordinates >= maximumX) {
+        if (this.xCoordinates - this.xSpeed <= minimumX) this.xCoordinates = minimumX;
+        else if (this.xCoordinates - this.xSpeed >= maximumX) this.xCoordinates = maximumX;
+        this.xSpeed *= -1;
+        this.xCoordinates += this.xSpeed;
+      } else if (this.yCoordinates <= minimumY || this.yCoordinates >= maximumY) {
+        if (this.yCoordinates - this.ySpeed <= minimumY) this.yCoordinates = minimumY;
+        else if (this.yCoordinates - this.ySpeed >= maximumY) this.yCoordinates = maximumY;
+        this.ySpeed *= -1;
+        this.yCoordinates += this.ySpeed;
+      } else {
+        this.xCoordinates += this.xSpeed;
+        this.yCoordinates += this.ySpeed;
+      }
     } else {
       throw new Error('Canvas context is null');
     }
   }
 
-  correctXCoordinates(rawXCoordinates: number): number {
+  private correctXCoordinates(rawXCoordinates: number): number {
     let result = rawXCoordinates;
     if (rawXCoordinates < this.radius) result = this.radius;
     else if (rawXCoordinates + this.radius > 300) result = 300 - this.radius;
     return result;
   }
 
-  correctYCoordinates(rawYCoordinates: number): number {
+  private correctYCoordinates(rawYCoordinates: number): number {
     let result = rawYCoordinates;
     if (rawYCoordinates < this.radius) result = this.radius;
     else if (rawYCoordinates + this.radius > 150) result = 150 - this.radius;
     return result;
+  }
+
+  private getContextBoundaries() {
+    const contextWidth = 300;
+    const contextHeight = 150;
+    const minimumX = this.radius;
+    const maximumX = contextWidth - this.radius;
+    const minimumY = this.radius;
+    const maximumY = contextHeight - this.radius;
+    return {
+      minimumX,
+      maximumX,
+      minimumY,
+      maximumY,
+    };
   }
 }
 
